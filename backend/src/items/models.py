8344ef2c8914b24e7841e26eea291daded71ac1e
Syed Farhan <syedfarhan596@gmail.com>
2024-02-13 21:03:17 +0530

    Delete backend/src directory
    delete src

--- backend/src/items/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Items(models.Model):
-    img = models.ImageField(upload_to='media')
-    name = models.CharField(max_length=200)
-    price = models.CharField(max_length=10)
-
-    def __str__(self):
-        return f"{self.name} {self.id}"
--- backend/src/items/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import ItemView
-urlpatterns = [
-    path('items/',ItemView.as_view(),name="items")
-]
